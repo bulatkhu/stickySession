b57a15f80535e676613c839d7b47bcda9ac6a25f
kiyasov <islam7407@gmail.com>
2018-11-08 20:02:36 +0300

    test

--- example/index.js
+++ example/index.js
@@ -1,14 +1,32 @@
 import http from "http";
 import cluster from "cluster";
+import express from "express";
+import multipart from "connect-multiparty";
 import StickySession from "../lib";
+
+const upload = multipart();
 
 let stickySession = new StickySession({
   listen: [3000],
   workers: 1
 });
 
-let server = require("http").createServer(function(req, res) {
-  res.end("worker: " + cluster.worker.id);
+const app = express();
+
+let server = http.Server(app);
+
+app.get("/", (req, res) => {
+  res.send(`worker id ${cluster.worker.id}`);
+});
+
+app.get("/upload", (req, res) => {
+  res.send(
+    `<form enctype="multipart/form-data" method="post"><input class="form-control" name="images" placeholder="Картинка" type="file"><button type="submit">Upload</button></form>`
+  );
+});
+
+app.post("/upload", upload, (req, res) => {
+  res.json(req.files);
 });
 
 if (!stickySession.listen(server)) {
